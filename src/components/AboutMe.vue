ebaeded5908e45fb8deb92624d5fe4e7c6dd6437
himanoa <matsunoappy@gmail.com>
2017-12-23 09:03:55 +0900

    Fix Flex wrap break

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -4,8 +4,8 @@
     <p>東京でWebエンジニアをしています。最近はテキストエディタのプラグイン開発とかをやります。来世は木か石になりたいです</p>
     <div class="skills">
       <h2>Skills</h2>
+      <h3>👍ﾜﾀｼﾁｮｯﾄﾃﾞｷﾙ</h3>
       <div class="good">
-        <h3>👍ﾜﾀｼﾁｮｯﾄﾃﾞｷﾙ</h3>
         <span class="tag">JavaScript</span>
         <span class="tag">Riot.js</span>
         <span class="tag">Vue.js</span>
@@ -17,8 +17,8 @@
         <span class="tag">Ruby on Rails</span>
         <span class="tag">Itamae</span>
       </div>
+      <h3>👀興味がある</h3>
       <div class="curios">
-        <h3>👀興味がある</h3>
         <span class="tag">Haskell</span>
         <span class="tag">Scala</span>
         <span class="tag">TypeScript</span>
@@ -40,16 +40,27 @@
   justify-content: center;
   align-items: center;
   flex-flow: column;
+  flex-wrap: wrap;
 }
 .skills {
 
 }
 
 .skills .good{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: row;
+  flex-wrap: wrap;
   margin-bottom: 1.5em;
 }
 
 .skills .curios{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: row;
+  flex-wrap: wrap;
   margin-bottom: 1.5em;
 }
 
@@ -59,6 +70,8 @@
 .tag {
   background-color: #52B3D9;
   border-radius: 5px;
+  margin-right: 0.4em;
+  margin-bottom: 0.4em;
   padding: 0.25em;
   color: #2C3E50;
   justify-content: flex-start;
